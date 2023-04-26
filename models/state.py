fe1f5e22562cb0d886efd3203efceed3fe094c75
lynnagidza <lynnagidza@gmail.com>
2023-04-26 17:32:13 +0300

    AirBnB clone - MySQL tasks

--- models/state.py
+++ models/state.py
@@ -1,8 +1,38 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""This is the state class"""
+from os import getenv
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy import Column, String
+from models.city import City
 
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    class State(BaseModel, Base):
+        """This is the class for State
+        Attributes:
+            name: input name
+        """
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship(
+            "City",
+            cascade="all,delete-orphan",
+            backref=backref("state", cascade="all"),
+            passive_deletes=True,
+            single_parent=True)
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+elif getenv("HBNB_TYPE_STORAGE") == "fs":
+    class State(BaseModel, Base):
+        """This is the class for State
+        Attributes:
+            name: input name
+        """
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+
+        @property
+        def cities(self):
+            """returns list of City instances with state_id"""
+            from models import storage
+            return [city for city in storage.all(City).values()
+                    if city.state_id == self.id]
